023ad12fd4317fbe01f401788e1ca65e165f62e8
Juan8516 <juan.ind85@gmail.com>
2025-05-22 19:47:51 -0500

    fix: Fix a mail record

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,48 +1,52 @@
-let loginForm = document.querySelector(".aside__login");
-let inputName = document.querySelector("#name");
-let inputEmail = document.querySelector("#email");
-let inputPassword = document.querySelector("#pass");
-let asideData = document.querySelector(".aside__data");
-let loginName = document.querySelector(".data__name");
-let btnLogout = document.querySelector(".data__logout");
+window.addEventListener("DOMContentLoaded", () => {
+    let loginForm = document.querySelector(".aside__login");
+    let inputName = document.querySelector("#name");
+    let inputEmail = document.querySelector("#email");
+    let inputPassword = document.querySelector("#pass");
+    let asideData = document.querySelector(".aside__data");
+    let loginName = document.querySelector(".data__name");
+    let btnLogout = document.querySelector(".data__logout");
 
-loginForm.addEventListener("submit", (e) => {
+    loginForm.addEventListener("submit", (e) => {
 
-    e.preventDefault();
+        e.preventDefault();
 
-    //Recoger los valores de formulario
-    let name = inputName.value;
-    let email = inputEmail.value;
-    let password = inputPassword.value;
+        //Recoger los valores de formulario
+        let name = inputName.value;
+        let email = inputEmail.value;
+        let password = inputPassword.value;
 
-    //Comprobar que todos los campos estan diligenciados 
-    let user = {};
+        //Comprobar que todos los campos estan diligenciados 
+        let user = {};
 
-    if(name && email && password) {
+        if(name && email && password) {
 
-        //Guardar los datos en un objeto
-        user = {name, email, password};
+            //Guardar los datos en un objeto
+            user = {name, email, password};
 
-        //Guardar en el localstore
-        localStorage.setItem("user", JSON.stringify(user));
+            //Guardar en el localstore
+            localStorage.setItem("user", JSON.stringify(user));
 
-        //Vaciar el formulario
-        loginForm.reset();
+            //Vaciar el formulario
+            loginForm.reset();
 
-        //Mostrar el usuario
-        getUser();
-    }
-});
+            //Mostrar el usuario
+            getUser();
+        }
+    });
 
-let getUser = () => {
-    let myUser = localStorage.getItem("user");
+    let getUser = () => {
+        let myUser = localStorage.getItem("user");
 
-    if(myUser) {
-        let identity = JSON.parse(myUser);
+        if(myUser) {
+            let identity = JSON.parse(myUser);
 
-        loginName.innerHTML = identity.name;
-        loginForm.classList.add("aside__login--hide");
-        asideData.classList.remove("aside__data--hide");
-    }
-};
+            loginName.innerHTML = identity.name;
+            loginForm.classList.add("aside__login--hide");
+            asideData.classList.remove("aside__data--hide");
+        }
+    };
+})
+
+
 
